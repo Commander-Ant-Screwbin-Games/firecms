262810bc1a8dc4d38f4f9604c3f3f89356d054b8
omatamix <47761650+omatamix@users.noreply.github.com>
2019-12-13 16:36:24 -0500

    Update MailerTest.php

--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -1,1 +1,13 @@
+<?php
+declare(strict_types=1);
 
+namespace FireCMS\Test;
+
+use FireCMS\Core\Mailer;
+
+/**
+ * Test the mailer component functionality.
+ */
+class MailerTest extends TestCase
+{
+}
